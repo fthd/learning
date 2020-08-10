00bff59ca0a857e7077e19fa53faed12e78119c0
tangjx <tangjx@koal.com>
2020-08-10 16:53:34 +0800

    初始化项目

---
+++ learning-parent/learning-tkzc-agent/src/main/java/com/learning/TkzcAgentApplication.java
@@ -0,0 +1,17 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * TkzcAgent启动类
+ *
+ * @author tangjx
+ * @date 2020/8/10 16:44
+ */
+public class TkzcAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkzcAgentApplication.class, args);
+    }
+
+}
